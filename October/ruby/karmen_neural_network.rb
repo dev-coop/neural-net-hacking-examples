c48bea3b1d90ab9432928403c17249520af6f019
Eric Carmichael <eric@ckcollab.com>
2015-10-28 20:05:43 -0700

    adds connection and such

--- October/ruby/karmen_neural_network.rb
+++ October/ruby/karmen_neural_network.rb
@@ -1,17 +1,59 @@
-class Neuron
-  attr_accessor :input, :output
+class Connection
+  attr_reader :source
 
-  def activation_function(input)
-    Math.sin(input)
+  def initialize(source, target)
+    @source = source # neuron
+    @target = target # neuron
+    @weight = 0.5
   end
 
-  def activate
+  def to_s
+    "Connection: #{@source} #{@target} #{@weight}"
+  end
+end
+
+class Neuron
+  attr_accessor :input
+  attr_reader :output, :incoming, :outgoing
+
+  def initialize
+    @incoming = []
+    @outgoing = []
+  end
+
+  def activation_function(input)
+    1 / (1+ Math.exp(-input))
+  end
+
+  def activate(value = nil)
+    input = value || incoming.reduce(0) do |sum, connection|
+      sum + connection.source.output
+    end
+
     @output = activation_function(input)
+  end
+
+  def connect(target)
+    connection = Connection.new(self, target)
+    outgoing << connection
+    target.incoming << connection
   end
 end
 
 
-neuron = Neuron.new
-neuron.input = 1
-neuron.activate
-puts neuron.output
+neuronA = Neuron.new
+neuronB = Neuron.new
+#neuronA.input = 2
+#neuronA.activate(10)
+#puts neuronA.output
+
+neuronA.connect(neuronB)
+
+neuronA.activate(10)
+puts neuronA.output
+
+neuronB.activate
+puts neuronB.output
+
+#puts neuronA.outgoing
+#puts neuronB.incoming
